6a8a29899cada9bdbaefe486d8989a20eebe460a
samurai <475331388@qq.com>
2013-10-25 11:43:31 +0800

    可配置，定时通过邮件发送报表

---
+++ src/tool/mail.go
@@ -0,0 +1,28 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: samurai
+ * Date: 13-7-1
+ * Time: 上午11:16
+ * To change this template use File | Settings | File Templates.
+ */
+package tool
+
+import (
+	"net/smtp"
+	"strings"
+)
+
+func SendMail(user, password, host, to, subject, body, mailtype string) error {
+	hp := strings.Split(host, ":")
+	auth := smtp.PlainAuth("", user, password, hp[0])
+	var content_type string
+	if mailtype == "html" {
+		content_type = "Content-Type: text/" + mailtype + "; charset=UTF-8"
+	} else {
+		content_type = "Content-Type: text/plain" + "; charset=UTF-8"
+	}
+	msg := []byte("To: " + to + "\r\nFrom: " + user + "<" + user + ">\r\nSubject: " + subject + "\r\n" + content_type + "\r\n\r\n" + body)
+	send_to := strings.Split(to, ";")
+	err := smtp.SendMail(host, auth, user, send_to, msg)
+	return err
+}
