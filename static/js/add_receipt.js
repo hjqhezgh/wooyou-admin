a195d4a6ade3cc11974d24d091dec4c882ef8174
Bill <cailiheng536@163.com>
2014-03-19 14:17:25 +0800

    * test ztree.

--- static/js/add_receipt.js
+++ static/js/add_receipt.js
@@ -2,12 +2,78 @@ define(function (require, exports, module) {
     var $ = require('jquery');
     require('juicer');
     require('timepicker');
+    require('/js/lib/ztree/jquery.ztree.all-3.5.min.js');
+    require('/js/lib/ztree/zTreeStyle.css');
+	var setting = { view: { dblClickExpand: false }, data: { simpleData: { enable: true } }, callback: { beforeClick: beforeClick, onClick: onClick } };
+
+		var zNodes =[
+			{id:1, pId:0, name:"北京", accounting_subject_code:"4567"},
+			{id:2, pId:0, name:"天津"},
+			{id:3, pId:0, name:"上海"},
+			{id:6, pId:0, name:"重庆"},
+			{id:4400, pId:0, name:"河北省", open:true},
+			{id:440001, pId:4400, name:"石家庄"},
+			{id:440002, pId:4400, name:"保定"},
+			{id:440003, pId:4400, name:"邯郸"},
+			{id:440004, pId:4400, name:"承德"},
+			{id:5, pId:0, name:"广东省", open:true},
+			{id:51, pId:5, name:"广州"},
+			{id:52, pId:5, name:"深圳"},
+			{id:53, pId:5, name:"东莞"},
+			{id:54, pId:5, name:"佛山"},
+			{id:6, pId:0, name:"福建省", open:true},
+			{id:61, pId:6, name:"福州"},
+			{id:62, pId:6, name:"厦门"},
+			{id:63, pId:6, name:"泉州"},
+			{id:64, pId:6, name:"三明"}
+		 ];
+
+		function beforeClick(treeId, treeNode) {
+			var check = (treeNode && !treeNode.isParent);
+			if (!check) alert("只能选择子节点...");
+			return check;
+		}
+		
+		function onClick(e, treeId, treeNode) {
+			var zTree = $.fn.zTree.getZTreeObj("treeDemo"),
+			nodes = zTree.getSelectedNodes(),
+			v = "";
+			code = "";
+			nodes.sort(function compare(a,b){return a.id-b.id;});
+			for (var i=0, l=nodes.length; i<l; i++) {
+				v += nodes[i].name + ",";
+				code = nodes[i].code;
+			}
+			if (v.length > 0 ) v = v.substring(0, v.length-1);
+			var cityObj = $(".ztreeInput");
+			cityObj.attr("value", v);
+//			cityObj.attr("name", accounting_subject_code);
+			hideMenu();
+		}
+
+		function showMenu() {
+			var cityObj = $(".ztreeInput");
+			var cityOffset = $(".ztreeInput").offset();
+			$("#menuContent").css({left:cityOffset.left + "px", top:cityOffset.top + cityObj.outerHeight() + "px"}).slideDown("fast");
+
+			$("body").bind("mousedown", onBodyDown);
+		}
+		function hideMenu() {
+			$("#menuContent").fadeOut("fast");
+			$("body").unbind("mousedown", onBodyDown);
+		}
+		function onBodyDown(event) {
+			if (!(event.target.id == "ztreeInput" || event.target.id == "menuContent" || $(event.target).parents("#menuContent").length>0)) {
+				hideMenu();
+			}
+		}
     
     var currentEmployee    = {};
     var accountingSubjects = {};
     var headquartersId     = 9;
     var process = {
 		init : function(){
+			$.fn.zTree.init($("#treeDemo"), setting, zNodes);
 			process.getCurrentEmployee();
 			process.setDepartments();
 			process.setAccountingSubject();
@@ -99,6 +165,8 @@ define(function (require, exports, module) {
 		},
 		bind : function(){
 			$('input.date').datepicker({ dateFormat : 'yy-mm-dd' });
+			$('input.ztreeInput').bind('click', showMenu); 
+	
 
 			var itemIndex = 1;
 			$('#addItem').click(function(e){
